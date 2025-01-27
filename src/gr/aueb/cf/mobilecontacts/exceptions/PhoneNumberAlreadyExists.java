5e0ab770eed62e87af8bb175267126cd0f90478e
evaperisteri <evangelia.peristeri.g@gmail.com>
2025-01-27 21:06:53 +0200

    initial commit

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExists.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class PhoneNumberAlreadyExists extends Exception {
+
+    public PhoneNumberAlreadyExists(String message){
+        super(message);
+    }
+}
